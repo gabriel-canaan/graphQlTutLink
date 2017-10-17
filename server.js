4dbc9aa7cbadaf51c2ed48adde57fae1dbcf9422
gabriel <777canaan@gmail.com>
2017-10-17 23:32:52 +1300

    close for business

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Graphql and Relay modern whoo hoo!')
 })
+
+const root = { friend: (args) => {
+  return {
+    'id': 23485676,
+    'firstName': 'dave',
+    'lastName': 'smith',
+    'gender': 'female',
+    'language': 'cuniform',
+    'email': 'dave@babyalon.ir',
+    'IDTyped': args.id
+  }
+}}
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}))
 app.listen(8080, () => console.log('Running on localhost:8080/graphql'))
