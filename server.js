c915b43a501f346c7d1b104a8db161948f857226
gabriel <777canaan@gmail.com>
2017-10-21 18:21:46 +1300

    crypto id created

--- server.js
+++ server.js
@@ -5,24 +5,39 @@ import schema from './schema'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Graphql and Relay modern whoo hoo!')
+    res.send('GraphQL & Relay modern boyee!')
 })
 
-const root = { friend: (args) => {
-  return {
-    'id': 23485676,
-    'firstName': 'dave',
-    'lastName': 'smith',
-    'gender': 'female',
-    'language': 'cuniform',
-    'email': 'dave@babyalon.ir',
-    'IDTyped': args.id
-  }
-}}
+class Friend {
+    constructor(id, {firstName, lastName, gender, language, email}) {
+        this.id = id
+        this.firstName = firstName
+        this.lastName = lastName
+        this.gender = gender
+        this.language = language
+        this.email = email
+    }
+}
 
+const friendDatabase = {}
+
+const global = {
+    getFriend: ({id}) => {
+        return new Friend(id, friendDatabase[id])
+    },
+    createFriend: ({input}) => {
+        let id = require('crypto').randomBytes(10).toString('hex')
+        friendDatabase[id] = input
+        return new Friend(id, input)
+    },
+    updateFriend: ({id, input}) => {
+        friendDatabase[id] = input
+        return new Friend(id, input)
+    }
+}
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
+    schema: schema,
+    rootValue: global,
+    graphiql: true,
 }))
-app.listen(8080, () => console.log('Running on localhost:8080/graphql'))
+app.listen(8080, () => console.log('Running server on localhost:8080/graphql'))
