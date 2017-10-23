830beec07e9d497bee5f709e8a7a0acb2617f1e1
gabriel <777canaan@gmail.com>
2017-10-23 17:44:50 +1300

    components setup

--- server.js
+++ server.js
@@ -1,43 +1,42 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
-import schema from './schema'
+import path from 'path'
+import webpack from 'webpack'
+import WebPackDevServer from 'webpack-dev-server'
+import { schema } from './data/schema'
 
-const app = express()
+const APP_PORT = 3000
+const GRAPHQL_PORT = 8080
 
-app.get('/', (req, res) => {
-    res.send('GraphQL & Relay modern boyee!')
-})
+const graphQLServer = express()
 
-class Friend {
-    constructor(id, {firstName, lastName, gender, language, email}) {
-        this.id = id
-        this.firstName = firstName
-        this.lastName = lastName
-        this.gender = gender
-        this.language = language
-        this.email = email
-    }
-}
-
-const friendDatabase = {}
-
-const global = {
-    getFriend: ({id}) => {
-        return new Friend(id, friendDatabase[id])
-    },
-    createFriend: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex')
-        friendDatabase[id] = input
-        return new Friend(id, input)
-    },
-    updateFriend: ({id, input}) => {
-        friendDatabase[id] = input
-        return new Friend(id, input)
-    }
-}
-app.use('/graphql', graphqlHTTP({
+graphQLServer.use('/', graphqlHTTP({
     schema: schema,
-    rootValue: global,
+    pretty: true,
     graphiql: true,
 }))
-app.listen(8080, () => console.log('Running server on localhost:8080/graphql'))
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`graphQLServer server on localhost:${GRAPHQL_PORT}`))
+
+const compiler = webpack({
+  entry: ['whatwg-fetch', path.resolve(__dirname, 'scr', 'App.js')],
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        test: /\.js$/,
+      },
+    ],
+  },
+  output: {filename: 'App.js', path: '/'}
+})
+
+const app = new WebPackDevServer(compiler, {
+  contentBase: '/public/',
+  proxy: {'graphql': `http://localhost:${APP_PORT}`},
+  publicPath: '/src',
+  stats: {colors: true},
+})
+
+app.use('/', express.static(path.resolve(__dirname, 'public')))
+app.listen(APP_PORT, () => console.log(`app server on localhost:${APP_PORT}`))
