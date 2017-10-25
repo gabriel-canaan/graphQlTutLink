83dec77648c947b31e18d8c0e31a9e1052b5bc78
gabriel <777canaan@gmail.com>
2017-10-25 22:40:29 +1300

    friends images showing

---
+++ data/database.js
@@ -0,0 +1,28 @@
+import data from './data.json'
+
+class User {}
+class Friend {}
+
+const viewer = new User()
+viewer.id = '1'
+viewer.name = 'me'
+const friends = data.map((obj) => {
+  const friend = new Friend()
+  friend.id = require('crypto').randomBytes(10).toString('hex')
+  friend.firstName = obj.firstName
+  friend.lastName = obj.lastName
+  friend.gender = obj.gender
+  friend.language = obj.language
+  friend.email = obj.email
+  friend.image = obj.image
+  return friend
+})
+
+module.exports = {
+  getUser: (id) => id === viewer.id ? viewer : null,
+  getViewer: () => viewer,
+  getFriend: (id) => friends.find(w => w.id == id),
+  getFriends: () => friends,
+  User,
+  Friend,
+}
--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-import express from 'express'
-import graphqlHTTP from 'express-graphql'
-import path from 'path'
-import webpack from 'webpack'
-import WebPackDevServer from 'webpack-dev-server'
-import { schema } from './data/schema'
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import path from 'path';
+import webpack from 'webpack';
+import WebPackDevServer from 'webpack-dev-server';
+import { schema } from './data/schema';
 
-const APP_PORT = 3000
-const GRAPHQL_PORT = 8080
+const APP_PORT = 3000;
+const GRAPHQL_PORT = 8080;
 
-const graphQLServer = express()
-
+// GraphQL server
+const graphQLServer = express();
 graphQLServer.use('/', graphqlHTTP({
     schema: schema,
     pretty: true,
     graphiql: true,
-}))
-graphQLServer.listen(GRAPHQL_PORT, () => console.log(`graphQLServer server on localhost:${GRAPHQL_PORT}`))
+}));
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL server on localhost:${GRAPHQL_PORT}`));
 
+// Relay
 const compiler = webpack({
-  entry: ['whatwg-fetch', path.resolve(__dirname, 'scr', 'App.js')],
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        test: /\.js$/,
-      },
-    ],
-  },
-  output: {filename: 'App.js', path: '/'}
-})
+    entry:['whatwg-fetch', path.resolve(__dirname, 'src', 'App.js')],
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                test: /\.js$/,
+            },
+        ],
+    },
+    output: {filename: 'App.js', path: '/'}
+});
 
 const app = new WebPackDevServer(compiler, {
-  contentBase: '/public/',
-  proxy: {'graphql': `http://localhost:${APP_PORT}`},
-  publicPath: '/src',
-  stats: {colors: true},
-})
+    contentBase: '/public/',
+    proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
+    publicPath: '/src/',
+    stats: {colors: true},
+});
 
-app.use('/', express.static(path.resolve(__dirname, 'public')))
-app.listen(APP_PORT, () => console.log(`app server on localhost:${APP_PORT}`))
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.listen(APP_PORT, () => console.log(`App is now running on localhost:${APP_PORT}`));
